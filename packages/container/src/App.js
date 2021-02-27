ac0a7fd80198d65ed9e505470e8aaf760947d676
Bung Danar <danar.g.putera@gmail.com>
2021-02-27 10:27:41 +0700

    End of Section 5

---
+++ packages/container/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { mount } from 'marketing/MarketingApp'
+import MarketingApp from './components/MarketingApp'
+
+console.log(mount)
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hi there, general Kennoby</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  )
+}
