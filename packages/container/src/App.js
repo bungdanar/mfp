e1f426e7b3e88d67a3cc42aa81352caa53b80be2
Bung Danar <danar.g.putera@gmail.com>
2021-03-01 09:08:14 +0700

    End of Section 9

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import { mount } from 'marketing/MarketingApp'
-import MarketingApp from './components/MarketingApp'
+import { BrowserRouter } from 'react-router-dom'
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles'
 
-console.log(mount)
+import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+})
 
 export default function App() {
   return (
-    <div>
-      <h1>Hi there!!!, general Kennoby</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   )
 }
