bf00059d6e97ebdcfa52b3ace1b6dbedc98b3b58
Bung Danar <danar.g.putera@gmail.com>
2021-03-02 09:17:34 +0700

    Added AuthApp

---
+++ packages/container/src/components/AuthApp.js
@@ -0,0 +1,29 @@
+import { mount } from 'auth/AuthApp'
+import React, { useEffect, useRef } from 'react'
+import { useHistory } from 'react-router-dom'
+
+export default function MarketingApp() {
+  const ref = useRef(null)
+  const history = useHistory()
+
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location
+
+        // console.log(pathname)
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname)
+        }
+      },
+    })
+
+    // Need to be run first time
+    onParentNavigate(history.location)
+
+    history.listen(onParentNavigate)
+  }, [])
+
+  return <div ref={ref} />
+}
